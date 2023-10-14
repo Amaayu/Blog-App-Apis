d9dcd53d662e2315440ec9465a676d31fafac37e
Amaayu <namdevaniket446@gmail.com>
2023-10-14 22:43:17 +0530

    first commit

---
+++ src/test/java/com/aashuwithcode/blogappapis/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashuwithcode.blogappapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
